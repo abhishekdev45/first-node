025debdb65f4a37246fd507fe45b09bae98f5134
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2023-10-31 14:30:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const { url } = req;
+
+  if (url === '/home') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome home\n');
+  } else if (url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to About Us page\n');
+  } else if (url === '/node') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to my Node.js project\n');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found\n');
+  }
+});
+
+server.listen(4000);
