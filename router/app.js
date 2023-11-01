fa44c7eeb4f8ebe6dec27903e941eaa994d41f6f
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2023-11-01 10:56:30 +0530

    Routers and Filters

---
+++ router/app.js
@@ -0,0 +1,19 @@
+// const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).send('<h1>page not found</h1>');
+})
+
+// const server = http.createServer(app);
+// server.listen(5000);
+app.listen(5000);
