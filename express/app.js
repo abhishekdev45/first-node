9e5f0dc3ace022bd4a007501ab96bbe2176b3867
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2023-11-01 10:18:50 +0530

    Update app.js

--- express/app.js
+++ express/app.js
@@ -1,15 +1,23 @@
 // const http = require('http');
-
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use((req,res,next) => {
-    console.log("in the middleware");
-    next();
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/add-product',(req,res,next) => {
+    // console.log("in the middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">add product</button></form>');
+});
+
+app.post('/product',(req,res,next) => {
+    console.log(req.body.title);
+    console.log(req.body.size);
+    res.redirect('/');
 })
-app.use((req,res,next) => {
-    console.log("in the next middleware");
+
+app.use('/',(req,res,next) => {
+    // console.log("in the next middleware");
     res.send('<h1>hello</h1>');
 })
 
